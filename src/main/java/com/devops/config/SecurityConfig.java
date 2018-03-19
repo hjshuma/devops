24682533e740e846387d0bd747469647fc49ee55
James Kim <hjshuma@gmail.com>
2018-03-19 20:55:19 +1100

    Created signup form and handler method

--- src/main/java/com/devops/config/SecurityConfig.java
+++ src/main/java/com/devops/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.devops.config;
 
 import com.devops.backend.service.UserSecurityService;
 import com.devops.web.controllers.ForgotMyPasswordController;
+import com.devops.web.controllers.SignupController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,7 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/error/**/*",
             "/console/**",
             ForgotMyPasswordController.FORGOT_PASSWORD_URL_MAPPING,
-            ForgotMyPasswordController.CHANGE_PASSWORD_PATH
+            ForgotMyPasswordController.CHANGE_PASSWORD_PATH,
+            SignupController.SIGNUP_URL_MAPPING
     };
 
     @Override
---
+++ src/main/java/com/devops/web/controllers/SignupController.java
@@ -0,0 +1,36 @@
+package com.devops.web.controllers;
+
+import com.devops.enums.PlansEnum;
+import com.devops.web.domain.frontend.ProAccountPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SignupController {
+
+    /** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(SignupController.class);
+
+    public static final String SIGNUP_URL_MAPPING = "/signup";
+
+    public static final String PAYLOAD_MODEL_KEY_NAME = "payload";
+
+    public static final String SUBSCRIPTON_VIEW_NAME = "registration/signup";
+
+    @RequestMapping(value = SIGNUP_URL_MAPPING, method = RequestMethod.GET)
+    public String signupGet(@RequestParam("planId") int planId, ModelMap model) {
+
+        if (planId != PlansEnum.BASIC.getId() && planId != PlansEnum.PRO.getId()) {
+            throw new IllegalArgumentException("Plan id is not valid");
+        }
+        model.addAttribute(PAYLOAD_MODEL_KEY_NAME, new ProAccountPayload());
+
+        return SUBSCRIPTON_VIEW_NAME;
+    }
+
+}
